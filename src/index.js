34368ab908bf7a3d8b3c9b27af82f1abdc8e6bcf
Anmol-7 <jainanmol5528@gmail.com>
2021-02-18 17:23:38 +0530

    minor deployment changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore,applyMiddleware,compose} from "redux";
+import { createStore,applyMiddleware} from "redux";
 import reduxThunk from 'redux-thunk';
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers,
-    composeEnhancers(applyMiddleware(reduxThunk))
+    applyMiddleware(reduxThunk)
     );
 
 ReactDOM.render(
